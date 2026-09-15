13ae20ddcc0645775b027c24d576f173c7e87f8f
saltoftheearth1 <crowley.liam@student.ccm.edu>
2026-09-15 19:29:20 -0400

    Made KeneticEnergy.java according to instructions

--- KineticEnergy.java
+++ KineticEnergy.java
@@ -1,3 +1,20 @@
-public class KineticEnergy {
+import java.util.Scanner;
 
+public class KineticEnergy {
+    // Kenetic Energy Method
+    private static double kineticEnergy(double mass,double velocity){
+        double KE = 0.5*mass*velocity*velocity;
+        return KE;
+    }
+    // get user input and then plug it into method
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Please enter the mass of the object: ");
+        double userM = input.nextDouble();
+        System.out.print("Please enter the velocity of the object: ");
+        double userV = input.nextDouble();
+        System.out.println(kineticEnergy(userM,userV));
+
+    }
 }
